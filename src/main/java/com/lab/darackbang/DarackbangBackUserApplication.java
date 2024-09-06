33c4c4ae1716ab2af17a8f4913e5337c1c1a343e
klaatus <klaatus@msn.com>
2024-09-06 21:09:17 +0900

    다락방유저백엔드 추가

---
+++ src/main/java/com/lab/darackbang/DarackbangBackUserApplication.java
@@ -0,0 +1,13 @@
+package com.lab.darackbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarackbangBackUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarackbangBackUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/darackbang/DarackbangBackUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.darackbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarackbangBackUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
