c5d3bf8faba02e09fe45525e9cf77fbe1d3fefa5
3270v <planetshining@naver.com>
2024-10-11 16:31:47 +0900

    1. 작성자: 이유민 2. 수정사항: - application.yml 이미지 저장 경로 수정 (집에서 테스트 시 @127.0.0.1:1521:xe로 변경 必) 3. 수정일: 2024-10-11 16:30

--- src/test/java/com/lab/darackbang/repository/MemberRepositoryTest.java
+++ src/test/java/com/lab/darackbang/repository/MemberRepositoryTest.java
@@ -21,33 +21,33 @@ public class MemberRepositoryTest {
     @Autowired
     private MemberRoleRepository memberRoleRepository;
 
-    @Test
-    public void insertMember() {
-        log.info("회원등록 테스트-----------------------------");
-
-        for (int i = 0; i < 5; i++) {
-            Member member = Member.builder()
-                    .userEmail("userJoin" + String.valueOf(i) + "@test.com")
-                    .password("1234")
-                    .name("user" + String.valueOf(i))
-                    .birthday("20240302")
-                    .ageGroup("20")
-                    .gender("F")
-                    .mobileNo("01028810137")
-                    .mileage(0)
-                    .build();
-            List<MemberRole> memberRoles = new ArrayList<>();
-
-            MemberRole userRole = new MemberRole();
-            userRole.setRole(Role.USER);
-            userRole.setMember(member);
-            memberRoles.add(userRole);
-
-            member.setMemberRoles(memberRoles);
-
-            memberRepository.save(member);
-        }
-    }
+//    @Test
+//    public void insertMember() {
+//        log.info("회원등록 테스트-----------------------------");
+//
+//        for (int i = 0; i < 5; i++) {
+//            Member member = Member.builder()
+//                    .userEmail("userJoin" + String.valueOf(i) + "@test.com")
+//                    .password("1234")
+//                    .name("user" + String.valueOf(i))
+//                    .birthday("20240302")
+//                    .ageGroup("20")
+//                    .gender("F")
+//                    .mobileNo("01028810137")
+//                    .mileage(0)
+//                    .build();
+//            List<MemberRole> memberRoles = new ArrayList<>();
+//
+//            MemberRole userRole = new MemberRole();
+//            userRole.setRole(Role.USER);
+//            userRole.setMember(member);
+//            memberRoles.add(userRole);
+//
+//            member.setMemberRoles(memberRoles);
+//
+//            memberRepository.save(member);
+//        }
+//    }
 
     @Test
     public void updateMember() { // 회원정보 수정 테스트
